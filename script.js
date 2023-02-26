16985a382a7bf3c1624a36d57b2dfdaf0c7b3e85
osmitee <obers735@newschool.edu>
2023-02-26 01:35:46 -0500

    ag-updates

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ function work() {
     let work = document.getElementById("work-screen");
     work.style.visibility = "visible"; 
 }
+
+function lipstick() {
+    let poster = document.getElementById("float");
+    let hide = document.getElementById("hide");
+    let show = document.getElementById("show");
+    poster.style.visibility = "hidden";
+    hide.style.visibility = "hidden";
+    show.style.visibility = "visible";
+}
+
+function lipstickshow() {
+    let poster = document.getElementById("float");
+    let hide = document.getElementById("hide");
+    let show = document.getElementById("show");
+    poster.style.visibility = "visible";
+    hide.style.visibility = "visible";
+    show.style.visibility = "hidden";
+}
