79bc83b05546634564f3a9cb7d63bf3cadd2231d
osmitee <obers735@newschool.edu>
2023-03-01 22:25:42 -0500

    wikibook-cyber

--- script.js
+++ script.js
@@ -20,3 +20,21 @@ function lipstickshow() {
     hide.style.visibility = "visible";
     show.style.visibility = "hidden";
 }
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
